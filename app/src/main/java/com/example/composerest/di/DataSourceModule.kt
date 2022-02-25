0e0c8275d4c895d8784192a700a21ef0a0bdaef9
Douglas Gomes <douglasgommez@gmail.com>
2022-02-25 19:16:17 -0300

    create di for database source

--- app/src/main/java/com/example/composerest/di/DataSourceModule.kt
+++ app/src/main/java/com/example/composerest/di/DataSourceModule.kt
@@ -1,9 +1,13 @@
 package com.example.composerest.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.composerest.datasource.DbDataSource
 import com.example.composerest.datasource.RestDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -32,5 +36,13 @@ class DataSourceModule {
     @Provides
     fun restDataSource(retrofit: Retrofit) : RestDataSource =
         retrofit.create(RestDataSource::class.java)
+
+    @Singleton
+    @Provides
+    fun dbDataSource(@ApplicationContext context: Context) : DbDataSource {
+        return Room.databaseBuilder(context, DbDataSource::class.java, "user_database")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 
 }
