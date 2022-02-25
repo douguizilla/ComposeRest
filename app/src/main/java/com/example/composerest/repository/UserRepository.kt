b137af25b57d8908e0072f8c3ee0d7396054abe1
Douglas Gomes <douglasgommez@gmail.com>
2022-02-25 19:22:27 -0300

    implement delete and getAll

--- app/src/main/java/com/example/composerest/repository/UserRepository.kt
+++ app/src/main/java/com/example/composerest/repository/UserRepository.kt
@@ -1,22 +1,33 @@
 package com.example.composerest.repository
 
+import androidx.lifecycle.LiveData
 import com.example.composerest.datasource.RestDataSource
 import com.example.composerest.model.User
+import com.example.composerest.model.UserDao
 import javax.inject.Inject
 
 interface UserRepository {
     suspend fun getNewUser() : User
+    suspend fun deleteUser(toDelete: User)
+    fun getAllUsers() : LiveData<List<User>>
 }
 
 class UserRepositoryImp @Inject constructor(
-    private val dataSource: RestDataSource
+    private val dataSource: RestDataSource,
+    private val userDao: UserDao
 ) : UserRepository {
 
     override suspend fun getNewUser(): User {
         val name = dataSource.getUserName().results[0].name!!
         val location = dataSource.getUserName().results[0].location!!
         val picture = dataSource.getUserName().results[0].picture!!
-        return User(name.first, name.last, location.city, picture.thumbnail)
+        val user = User(name.first, name.last, location.city, picture.thumbnail)
+        userDao.inset(user)
+        return user
     }
+
+    override suspend fun deleteUser(toDelete: User) = userDao.delete(toDelete)
+
+    override fun getAllUsers(): LiveData<List<User>> = userDao.getAll()
 
 }
