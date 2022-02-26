c907b4d6e6dbc669c33d5af71710cc0e044d4452
Douglas Gomes <douglasgommez@gmail.com>
2022-02-26 19:41:55 -0300

    implement UserViewModel.kt

--- app/src/main/java/com/example/composerest/UserViewModel.kt
+++ app/src/main/java/com/example/composerest/UserViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.composerest
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.composerest.model.User
 import com.example.composerest.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -14,10 +17,31 @@ class UserViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
-    fun getUser(){
-        viewModelScope.launch(Dispatchers.IO) {
-            val user = userRepository.getNewUser()
-            Log.d("UserViewModel", user.toString())
+    private val _isLoading : MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>(false)
+    }
+
+    val users : LiveData<List<User>> by lazy {
+        userRepository.getAllUsers()
+    }
+
+    val isLoading : LiveData<Boolean> get() = _isLoading
+
+    fun addItem(){
+        if(_isLoading.value == false){
+            viewModelScope.launch(Dispatchers.IO) {
+                _isLoading.postValue(true)
+                userRepository.getNewUser()
+                _isLoading.postValue(false)
+            }
         }
     }
+
+    fun deleteItem(toDelete: User){
+        viewModelScope.launch(Dispatchers.IO) {
+            userRepository.deleteUser(toDelete)
+        }
+    }
+
+
 }
