0051ee4b02410a502140eaec9bd1d6faafc13f53
Douglas Gomes <douglasgommez@gmail.com>
2022-02-28 13:45:40 -0300

    skeleton loading function implemented

--- app/src/main/java/com/example/composerest/MainActivity.kt
+++ app/src/main/java/com/example/composerest/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.composerest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -12,10 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.composerest.model.User
 import com.example.composerest.ui.theme.ComposeRestTheme
+import com.valentinilk.shimmer.shimmer
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,7 +52,7 @@ fun MyApp1(
 
     MyApp(
         onAddClick = {
-        viewModel.addUser()
+            viewModel.addUser()
         },
         users = users,
         isLoading = isLoading
@@ -55,14 +61,14 @@ fun MyApp1(
 
 @Composable
 fun MyApp(
-    onAddClick : (() -> Unit)? = null,
+    onAddClick: (() -> Unit)? = null,
     users: List<User>,
-    isLoading : Boolean
+    isLoading: Boolean
 ) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Compose Rest + Room")},
+                title = { Text("Compose Rest + Room") },
                 actions = {
                     IconButton(onClick = {
                         onAddClick?.invoke()
@@ -73,15 +79,15 @@ fun MyApp(
             )
         }
     ) {
-        LazyColumn{
+        LazyColumn {
             var itemCount = users.size
-            if(isLoading) itemCount ++
+            if (isLoading) itemCount++
 
-            items(count = itemCount){ index ->
+            items(count = itemCount) { index ->
                 var auxIndex = index
-                if(isLoading){
-                    if(auxIndex == 0)
-                        return@items Text(text = "IsLoading")
+                if (isLoading) {
+                    if (auxIndex == 0)
+                        return@items LoadingCard()
                     auxIndex--
                 }
             }
@@ -89,10 +95,61 @@ fun MyApp(
     }
 }
 
+@Composable
+fun LoadingCard() {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 1.dp,
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()
+            .testTag("loadingCard")
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            ImageLoading()
+            Spacer()
+            Column {
+                Spacer()
+                Box(modifier = Modifier.shimmer()) {
+                    Column {
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                        Spacer()
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageLoading() {
+    Box(modifier = Modifier.shimmer()) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Gray)
+        )
+    }
+}
+
+@Composable
+fun Spacer(size: Int = 8) = Spacer(modifier = Modifier.size(size.dp))
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeRestTheme {
-        MyApp(onAddClick = null, users = emptyList(), isLoading = false)
+        MyApp(onAddClick = null, users = emptyList(), isLoading = true)
     }
 }
